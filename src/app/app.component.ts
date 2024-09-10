061949f5041b25e81559633d565b1991bf6eb1f7
ZOUHAYR BAHOUMI <bahoumizouhayr@gmail.com>
2024-09-10 16:04:48 +0100

    creation du navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { NavbarComponent } from "./shared/compenants/navbar/navbar.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, MatSlideToggleModule],
+  imports: [CommonModule, RouterOutlet, MatSlideToggleModule, NavbarComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
