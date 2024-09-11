4593076d8e683aad6c4d8ce1c10417997b52fc38
ZOUHAYR BAHOUMI <bahoumizouhayr@gmail.com>
2024-09-11 12:08:43 +0100

    terminer service afficher les projet

--- src/app/pages/projets/list-projet/list-projet.component.ts
+++ src/app/pages/projets/list-projet/list-projet.component.ts
@@ -1,5 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Projet } from 'src/app/interfaces/projet';
+import { ProjetService } from 'src/app/shared/services/projet.service';
+import { HttpErrorResponse } from '@angular/common/http';
 
 @Component({
   selector: 'app-list-projet',
@@ -8,6 +11,24 @@ import { CommonModule } from '@angular/common';
   templateUrl: './list-projet.component.html',
   styleUrls: ['./list-projet.component.scss']
 })
-export class ListProjetComponent {
+export class ListProjetComponent implements OnInit {
 
+  public projets: Projet[] = [];
+
+  constructor(private projetService : ProjetService){}
+
+  ngOnInit() {
+    this.getProjets();
+  }
+  public getProjets():void{
+    this.projetService.getProjets().subscribe(
+      (response : Projet[]) =>{
+        console.log(response);
+        this.projets= response;
+      },
+      (error : HttpErrorResponse) =>{
+        console.log(error.message);
+      }
+    );
+  }
 }
