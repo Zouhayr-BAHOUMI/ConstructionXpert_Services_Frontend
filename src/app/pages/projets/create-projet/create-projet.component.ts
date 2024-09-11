f6b96cabf62703e929ecf07e8f9a4771c98d6339
ZOUHAYR BAHOUMI <bahoumizouhayr@gmail.com>
2024-09-11 15:04:01 +0100

    adding fonction pour ajouter projet

--- src/app/pages/projets/create-projet/create-projet.component.ts
+++ src/app/pages/projets/create-projet/create-projet.component.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterModule } from '@angular/router';
-import { FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { Router, RouterModule } from '@angular/router';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { ProjetService } from 'src/app/shared/services/projet.service';
 
 @Component({
   selector: 'app-create-projet',
@@ -10,8 +11,41 @@ import { FormGroup, ReactiveFormsModule } from '@angular/forms';
   templateUrl: './create-projet.component.html',
   styleUrls: ['./create-projet.component.scss']
 })
-export class CreateProjetComponent {
+export class CreateProjetComponent implements OnInit{
 
   createProjetForm!: FormGroup;
+
+  constructor(
+    private projetService: ProjetService,
+    private formBuilder: FormBuilder,
+    private router: Router
+  ) {
+    this.createProjetForm = this.formBuilder.group({
+      nom_projet: ['', Validators.required],
+      description: ['', Validators.required],
+      date_debut: ['', Validators.required],
+      date_fin: ['', Validators.required],
+      budget: ['', [Validators.required, Validators.min(0)]]
+    });
+  }
+
+  ngOnInit(): void {}
+
+  onSubmit(): void {
+    console.log('Payload:', JSON.stringify(this.createProjetForm.value));
+    if (this.createProjetForm.valid) {
+      const projetToAdd = this.createProjetForm.value;
+
+      this.projetService.addProjet(projetToAdd).subscribe(
+        response => {
+          console.log('Project created successfully', response);
+          this.router.navigate(['']);
+        },
+        error => {
+          console.error('Error creating project', error);
+        }
+      );
+    }
+  }
 
 }
