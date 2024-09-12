c7d43afe799a3d36113cbb2230a252751018dd0e
ER-RAJY <noureddineerajy@gmail.com>
2024-09-12 09:36:13 +0100

    add component ed-tache

---
+++ src/app/pages/taches/create-tache/create-tache.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-create-tache',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './create-tache.component.html',
+  styleUrls: ['./create-tache.component.scss']
+})
+export class CreateTacheComponent {
+
+}
