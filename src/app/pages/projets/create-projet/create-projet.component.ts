5be4402e18589c112e62525fd0ed41633c375665
ER-RAJY <noureddineerajy@gmail.com>
2024-09-13 14:21:26 +0100

    add logic for create tache.ts

--- src/app/pages/projets/create-projet/create-projet.component.ts
+++ src/app/pages/projets/create-projet/create-projet.component.ts
@@ -2,49 +2,50 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Router, RouterModule } from '@angular/router';
 import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
-import { TacheService } from 'src/app/shared/services/tache.service';
+import { ProjetService } from 'src/app/shared/services/projet.service';
 
 @Component({
-  selector: 'app-create-tache',
+  selector: 'app-create-projet',
   standalone: true,
   imports: [CommonModule, RouterModule, ReactiveFormsModule],
-  templateUrl: './create-tache.component.html',
-  styleUrls: ['./create-tache.component.scss']
+  templateUrl: './create-projet.component.html',
+  styleUrls: ['./create-projet.component.scss']
 })
-export class CreateTacheComponent implements OnInit {
+export class CreateProjetComponent implements OnInit{
 
-  createTacheForm!: FormGroup;
+  createProjetForm!: FormGroup;
 
   constructor(
-    private tacheService: TacheService,
+    private projetService: ProjetService,
     private formBuilder: FormBuilder,
     private router: Router
   ) {
-    this.createTacheForm = this.formBuilder.group({
+    this.createProjetForm = this.formBuilder.group({
+      nom_projet: ['', Validators.required],
       description: ['', Validators.required],
       date_debut: ['', Validators.required],
       date_fin: ['', Validators.required],
-      status: ['', Validators.required],
-      idProjet: ['', [Validators.required, Validators.min(1)]]
+      budget: ['', [Validators.required, Validators.min(0)]]
     });
   }
 
   ngOnInit(): void {}
 
   onSubmit(): void {
-    if (this.createTacheForm.valid) {
-      const tacheToAdd = this.createTacheForm.value;
-      const idProjet = tacheToAdd.idProjet;
 
-      this.tacheService.addTache(tacheToAdd, idProjet).subscribe(
+    if (this.createProjetForm.valid) {
+      const projetToAdd = this.createProjetForm.value;
+
+      this.projetService.addProjet(projetToAdd).subscribe(
         response => {
-          console.log('Task created successfully', response);
-          this.router.navigate(['/tasks']); // Adjust this route as needed
+          console.log('Project created successfully', response);
+          this.router.navigate(['']);
         },
         error => {
-          console.error('Error creating task', error);
+          console.error('Error creating project', error);
         }
       );
     }
   }
+
 }
--- src/app/pages/taches/create-tache/create-tache.component.ts
+++ src/app/pages/taches/create-tache/create-tache.component.ts
@@ -1,13 +1,50 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Router, RouterModule } from '@angular/router';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { TacheService } from 'src/app/shared/services/tache.service';
 
 @Component({
   selector: 'app-create-tache',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule, ReactiveFormsModule],
   templateUrl: './create-tache.component.html',
   styleUrls: ['./create-tache.component.scss']
 })
-export class CreateTacheComponent {
+export class CreateTacheComponent implements OnInit {
 
+  createTacheForm!: FormGroup;
+
+  constructor(
+    private tacheService: TacheService,
+    private formBuilder: FormBuilder,
+    private router: Router
+  ) {
+    this.createTacheForm = this.formBuilder.group({
+      description: ['', Validators.required],
+      date_debut: ['', Validators.required],
+      date_fin: ['', Validators.required],
+      status: ['', Validators.required],
+      idProjet: ['', [Validators.required, Validators.min(1)]]
+    });
+  }
+
+  ngOnInit(): void {}
+
+  onSubmit(): void {
+    if (this.createTacheForm.valid) {
+      const tacheToAdd = this.createTacheForm.value;
+      const idProjet = tacheToAdd.idProjet;
+
+      this.tacheService.addTache(tacheToAdd, idProjet).subscribe(
+        response => {
+          console.log('Task created successfully', response);
+          this.router.navigate(['/tasks']); // Adjust this route as needed
+        },
+        error => {
+          console.error('Error creating task', error);
+        }
+      );
+    }
+  }
 }
